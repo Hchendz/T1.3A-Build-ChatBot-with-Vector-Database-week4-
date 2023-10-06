cf31a9c6bb0bfa2c7a1a39d798a791640f917250
Hchendz <144656283+Hchendz@users.noreply.github.com>
2023-10-06 15:00:20 +0800

    Update print_vectors.py

--- print_vectors.py
+++ print_vectors.py
@@ -1,11 +1,14 @@
 """
-打印pdf_index.ann文件中储存的所有向量
+Prints out all vectors stored in the pdf_index.ann file
 """
 
 from annoy import AnnoyIndex
 
+# 指定向量维数（要与glove词向量库维数保持一致）
+VECTOR_SIZE = 100
+
 # 加载Annoy索引
-index = AnnoyIndex(10, "angular")
+index = AnnoyIndex(VECTOR_SIZE, "angular")
 index.load("pdf_index.ann")
 
 # 获取向量数量
